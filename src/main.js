742317c3d73ee6b19236f57c1fda7f840ab12eae
saraHH1416 <z.haghighi19@gmail.com>
2022-04-05 15:03:44 +0430

    installing i18n for internationalization

--- src/main.js
+++ src/main.js
@@ -7,12 +7,13 @@ import { firebaseApp, getAuth, onAuthStateChanged } from './includes/firebase';
 import Icon from './directives/icon';
 import './assets/tailwind.css';
 import './assets/main.css';
+import i18n from './includes/i18n';
 
 let app;
 const auth = getAuth(firebaseApp);
 onAuthStateChanged(auth, () => {
   if (!app) {
-    app = createApp(App);
+    app = createApp(App).use(i18n);
     app.use(store);
     app.use(router);
     app.use(VeeValidationPlugin);
